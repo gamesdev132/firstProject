4c16585b7398a3be3136aeae7cf24163d8e793ab
MIGUELEZ MATTEO p2101471 <matteo.miguelez@etu.univ-lyon1.fr>
2024-06-16 23:12:50 +0200

    adding firebase

---
+++ dist/assets/PodiumView-BqJ-wAoX.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as s,a as t}from"./index-5Dxv0jQZ.js";const c={},n={class:"about"},_=t("h1",null,"Comming soon...(non)",-1),a=[_];function r(i,d){return e(),s("div",n,a)}const m=o(c,[["render",r]]);export{m as default};
